b33684b4a442f24de0ca327edd4d3483361b12b9
unknown <rmsmall7@gmail.com>
2019-05-26 09:38:31 -0400

    updated friends.js

---
+++ app/data/friend.js
@@ -0,0 +1,18 @@
+var friends = [
+  {
+    name: "Jimmy",
+    photo: "https://www.elitesingles.ie/sites/www.elitesingles.ie/files/styles/elite_rectangle_article_arrow_left_frame/public/2b_en_ta_teaser_sp1.png",
+    scores: [
+      "2",
+      "1",
+      "4",
+      "1",
+      "5",
+      "2",
+      "4",
+      "5",
+      "3",
+      "4"
+    ]
+  }
+]
