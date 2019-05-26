0d6bc805eb3e0c15debd685bacda23c04a5b4641
unknown <rmsmall7@gmail.com>
2019-05-26 09:53:31 -0400

    updated friend.js list

--- app/data/friend.js
+++ app/data/friend.js
@@ -14,5 +14,73 @@ var friends = [
       "3",
       "4"
     ]
-  }
+  },
+
+  {
+    name: "Lisa",
+    photo: "https://i.dailymail.co.uk/i/pix/2016/05/23/22/348B850600000578-3605456-image-m-32_1464040491071.jpg",
+    scores: [
+      "3",
+      "2",
+      "5",
+      "2",
+      "1",
+      "3",
+      "4",
+      "1",
+      "4",
+      "5"
+    ]
+  },
+
+  {
+    name: "Alan",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa4tOOL1qcA_bkkjw6T83V5RRGhBQ0BYjGeBnSA5Se6lbX6G7xmA",
+    scores: [
+      "4",
+      "3",
+      "1",
+      "3",
+      "2",
+      "4",
+      "5",
+      "2",
+      "5",
+      "1"
+    ]
+  },
+
+  {
+    name: "Marvin",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSFFpo0RQ-VX_-Ct1vtng1tkKlPFzOjzU7HP9oqw0vBKzPfCy6Kw",
+    scores: [
+      "5",
+      "4",
+      "2",
+      "4",
+      "3",
+      "5",
+      "1",
+      "3",
+      "1",
+      "2"
+    ]
+  },
+
+  {
+    name: "Carol",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC0PaKDUysFgPSv4jewnJx6yp4VD_tBQj81Ld2-ez7cQj4NMmF0w",
+    scores: [
+      "1",
+      "5",
+      "3",
+      "5",
+      "4",
+      "1",
+      "2",
+      "4",
+      "2",
+      "3"
+    ]
+  },
 ]
